9543c5a4cb050ef7cac79113d5adfe77a220d1c1
Arnau Mora <arnyminer.z@gmail.com>
2022-09-21 11:34:08 +0200

    :sparkles: Added reset state

--- entregable/disenyo_entregable/fifo.sv
+++ entregable/disenyo_entregable/fifo.sv
@@ -12,7 +12,8 @@ module
 fifo #(
   parameter [1:0] st_empty = 1'd0,
   parameter [1:0] st_full  = 1'd1,
-  parameter [1:0] st_other = 1'd2
+  parameter [1:0] st_other = 1'd2,
+  parameter [1:0] st_reset = 1'd3
 )(
   // Entradas
   input CLOCK, RESET_N, READ, WRITE, CLEAR_N, // Input flags
@@ -28,6 +29,11 @@ fifo #(
 reg [7:0] waddr, raddr; // aka countw, countr
 
 reg [7:0] DATA_OUT_RAM, DATA_OUT_INTERNAL;
+
+// Se usa esta flag ya que la RAM actualiza el valor de salida, en este
+// caso DATA_OUT_RAM. Cuando este valor se debe escribir en la salida,
+// se actualizará cp_ram a 1, para indicar que el valor de la salida
+// debe ser DATA_OUT_RAM y no DATA_OUT_INTERNAL
 reg cp_ram; // Indica que se debe usar el valor de la RAM
 
 // -- ESTADOS MÁQUINA --
@@ -36,12 +42,12 @@ reg [1:0] state, nextstate;
 // Declaración del módulo de memoria
 ram_dp #(.mem_depth(32), .size(8))
 RAM (
-  .data_in(DATA_IN),  // Datos de entrada
-  .wren(WRITE),       // Flag de escritura
-  .rden(READ),        // Flag de lectura
-  .clock(CLOCK),      // Señal de reloj
-  .wraddress(waddr),  // Dirección de escritura
-  .rdaddress(raddr),  // Dirección de lectura
+  .data_in(DATA_IN),      // Datos de entrada
+  .wren(WRITE),           // Flag de escritura
+  .rden(READ),            // Flag de lectura
+  .clock(CLOCK),          // Señal de reloj
+  .wraddress(waddr),      // Dirección de escritura
+  .rdaddress(raddr),      // Dirección de lectura
   .data_out(DATA_OUT_RAM) // Salida de datos
 );
 
@@ -50,7 +56,7 @@ RAM (
 always @(posedge CLOCK, negedge RESET_N, negedge CLEAR_N)
 begin
   if (!RESET_N || !CLEAR_N)
-    state <= st_empty;
+    state <= st_reset;
   else
     state <= nextstate;
 end
@@ -93,13 +99,28 @@ begin
       nextstate <= st_full;
     else
       nextstate <= st_other;
+
+    st_reset:
+      nextstate <= st_empty;
   endcase
 end
 
 // ! -- DATA PATH -- !
+// State machine
 always @(state)
 begin
   case (state)
+    st_reset:
+    begin
+      F_EMPTY_N <= 1'b0;         // Establecemos que la memoria está vacía
+      F_FULL_N  <= 1'b1;         // Establecemos que la memoria no está llena
+      cp_ram <= 1'b0;            // Reiniciamos la flag de copia de RAM
+      raddr <= 8'b0;             // Reiniciamos el contador del puntero de lectura
+      waddr <= 8'b0;             // Reiniciamos el contador del puntero de escritura
+      USE_DW <= 5'b0;            // Reiniciamos el contador de uso
+      DATA_OUT_INTERNAL <= 8'b0; // Reiniciamos la memoria de salida de datos
+    end
+
     st_empty:
     begin
       F_EMPTY_N <= 0;
@@ -115,7 +136,7 @@ begin
         begin
           waddr <= waddr+1;
           USE_DW <= USE_DW+1;
-          RAM[waddr] <= DATA_IN;
+          // RAM[waddr] <= DATA_IN;
         end
       end
     end
@@ -130,13 +151,13 @@ begin
         waddr <= waddr + 1;
         // DATA_OUT <= RAM[raddr];
         cp_ram <= 1; // Block write
-        RAM[waddr] <= DATA_IN;
+        // RAM[waddr] <= DATA_IN;
       end
       2'b10:
       begin
         waddr <= waddr + 1;
         USE_DW <= USE_DW+1;
-        RAM[waddr] <= DATA_IN;
+        // RAM[waddr] <= DATA_IN;
       end
       2'b01:
       begin
@@ -159,7 +180,7 @@ begin
           waddr <= waddr + 1;
           // DATA_OUT <= RAM[raddr];
           cp_ram <= 1; // Block write
-          RAM[waddr] <= DATA_IN;
+          // RAM[waddr] <= DATA_IN;
         end
         else
         begin
